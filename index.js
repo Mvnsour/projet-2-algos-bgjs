1375cc54475587a7214b15fc305498814e7c1089
Mansour Mbengue <mbgmansour13@gmail.com>
2025-02-02 12:00:02 +0100

    feat : algorithmic solution to find how many fishes there are after 80 days

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ import { readFileSync } from 'fs';
 const test = readFileSync('test.txt', 'utf8');
 const final = readFileSync('final.txt', 'utf8');
 
-console.log({ test, final });
+// console.log({ test, final });
+
+const part1 = (data) => {
+  const days = 80;
+  const fishes = data.split(',').map(Number);
+  
+  for (let day = 0; day < days; day++) {
+    const saveLength = fishes.length
+    for (let i = 0; i < saveLength; i++) {
+      if (fishes[i] === 0 ) {
+        fishes[i] = 6;
+        fishes.push(8)
+      } else {
+        fishes[i]--;
+      }
+    }
+  }
+  return {
+    fishes,
+    length: fishes.length,
+  };
+}
+
+console.log(part1(final));
